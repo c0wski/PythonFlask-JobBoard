3ade311d2328ca3774fdabc30e6714950e44d9d6
c0wski <100622825+c0wski@users.noreply.github.com>
2022-02-28 19:29:13 -0500

    completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = db/jobs.sqlite
 
 app = Flask(__name__)
+
+def open_connection()
+    getattr(g, _connection, None)
+        return connection
+    if connection == None:
+        sqlite3.connect(PATH) = connection, g._connection
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
 
 @app.route('/')
 @app.route('/jobs')
